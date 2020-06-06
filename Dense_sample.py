1fb8b30a0704f6f26773f1c539d56f97af09dc2c
wintertee <27325188+wintertee@users.noreply.github.com>
2020-06-06 17:04:00 +0800

    remove keras datasets, add MNIST dataset

--- Dense_sample.py
+++ Dense_sample.py
@@ -1,10 +1,14 @@
 from Re0_neural_network import activations, initializers, layers, losses, models, optimizers
 
-from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-(train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+mnist = np.load('datasets/mnist.npz')
+train_images = mnist['train_images']
+train_labels = mnist['train_labels']
+test_images = mnist['test_images']
+test_labels = mnist['test_labels']
+
 train_images = train_images.reshape(60000, 784).astype(float)
 train_images /= 255
 
@@ -17,8 +21,8 @@ model.append(layers.Dense(784, 64, activations.relu, initializers.He))
 model.append(layers.Dense(64, 10, activations.softmax, initializers.He))
 model.build()
 
-# model.config(optimizer=optimizers.SGD, loss=losses.Crossentropy, lr=0.001)
-model.config(optimizer=optimizers.PRBCD, loss=losses.Crossentropy, lr=0.001)
+model.config(optimizer=optimizers.SGD, loss=losses.Crossentropy, lr=0.001)
+# model.config(optimizer=optimizers.PRBCD, loss=losses.Crossentropy, lr=0.001)
 # model.config(optimizer=optimizers.RCD, loss=losses.Crossentropy, lr=0.001, n=10000)
 
 losses = []
